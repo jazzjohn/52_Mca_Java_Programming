c76442ce754385b1f2d3c53a121e3d770b5c3aa5
jazzjohn <jazzjohn1999@gmail.com>
2021-09-14 12:41:05 +0530

    Some files modified

--- CO4/pgm5/ThreadClass.java
+++ CO4/pgm5/ThreadClass.java
@@ -1,63 +1,58 @@
 import java.util.*;
-import java.lang.*;
 
 class MultiplicationTable extends Thread {
-    int num;
-
-    MultiplicationTable(Scanner read) {
-        System.out.print("\nEnter the number for multiplication table: ");
-        num = read.nextInt();
-    }
-
-    synchronized public void multiplication() {
-        System.out.println("\nMultiplication table of " + num + " : ");
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i + "*" + num + " = " + (i * num));
-        }
-    }
 
     @Override
     public void run() {
-        multiplication();
+        System.out.println("\nMultiplication table of 5 is : \n");
+        try {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(i + "*5 = " + (i * 5));
+                Thread.sleep(1000);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
 
 class primeNumbers extends Thread {
     int limit;
+    Scanner read = new Scanner(System.in);
 
     primeNumbers(Scanner read) {
         System.out.print("\nEnter the limit for prime numbers: ");
         limit = read.nextInt();
     }
 
-    synchronized public void prime() {
-        System.out.println("\nThe prime numbers upto " + limit + " is : \n");
-        for (int i = 2, count = 1; count <= limit; i++) {
-            int flag = 0;
-            for (int j = 2; j <= Math.sqrt(i); j++) {
-                if (i % j == 0) {
-                    flag = 1;
-                    break;
-                }
-            }
-            if (flag == 0) {
-                System.out.println(i);
-                count += 1;
-            }
-        }
-
-    }
-
     @Override
     public void run() {
-        prime();
+        System.out.println("\nThe prime numbers upto " + limit + " is : \n");
+        try {
+            for (int i = 2, count = 1; count <= limit; i++) {
+                int flag = 0;
+                for (int j = 2; j <= Math.sqrt(i); j++) {
+                    if (i % j == 0) {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0) {
+                    System.out.println(i);
+                    count += 1;
+                }
+                Thread.sleep(1000);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
 
 public class ThreadClass {
     public static void main(String[] ars) {
         Scanner read = new Scanner(System.in);
-        MultiplicationTable mObj = new MultiplicationTable(read);
+        MultiplicationTable mObj = new MultiplicationTable();
         primeNumbers pObj = new primeNumbers(read);
         mObj.start();
         pObj.start();
--- CO4/pgm6/RunnableInterface.java
+++ CO4/pgm6/RunnableInterface.java
@@ -4,20 +4,19 @@ class FibonacciNums implements Runnable {
     int limit;
 
     FibonacciNums(Scanner read) {
-        System.out.print("\nEnter the limit for Fibonacci numbers: ");
+        System.out.print("\nEnter the range for Fibonacci numbers: ");
         limit = read.nextInt();
     }
 
-    synchronized public void display(){
+    synchronized public void display() {
         System.out.println("\nFibonacci numbers are: ");
-        for (int i = 1, j = 1, count = 1; count <= limit; count++) {
+        for (int i = 1, j = 1; i <= limit;) {
             System.out.println(i);
             j = i + j;
             i = j - i;
             j = (j - i) + i;
         }
     }
-    
 
     @Override
     public void run() {
@@ -33,7 +32,7 @@ class evenNums implements Runnable {
         limit = read.nextInt();
     }
 
-    synchronized public void display(){
+    synchronized public void display() {
         System.out.println("\nThe Even numbers upto " + limit + " is : ");
         for (int i = 1; i <= limit; i++) {
             if (i % 2 == 0) {
